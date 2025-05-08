032b39dd9ddcb8973f4411b22cb8edba542be886
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-05-08 09:42:10 +0530

    day 2

---
+++ day2/problem1.java
@@ -0,0 +1,54 @@
+package day2;
+
+public class problem1 import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void checkMagazine(List<String> magazine, List<String> note) {
+        for (String word : note) {
+            if (magazine.contains(word)) {
+                magazine.remove(word);
+            } else {
+                System.out.println("No");
+                return;
+            }
+        }
+
+        System.out.println("Yes");
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(firstMultipleInput[0]);
+
+        int n = Integer.parseInt(firstMultipleInput[1]);
+
+        List<String> magazine = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+
+        List<String> note = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+
+        Result.checkMagazine(magazine, note);
+
+        bufferedReader.close();
+    }
+}
+{
+}
